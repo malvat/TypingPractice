bc96de0a82ae426a839f2add8b6f3504d6066272
malvat <malvatanim@gmail.com>
2020-07-27 00:07:17 -0400

    add functionality and bug fix

--- js/main.js
+++ js/main.js
@@ -1,31 +1,123 @@
+var current = 0;
+var color = "#3f51b5";
+var content = "It's not enough to hire to fill a job. It's not even enough to hire on the basis of one's talents. You have to hire based upon a candidate's potential to grow and develop.";    
+var words = content.split(" ");
+var seconds = 0;
+var start_game = false;
+var timer;
+var streak = 1;
+var score = 0;
+var score_posted = 0;
+var speed = 0;
+var end_game = false;
+var score_speed = 100;
+var score_update;
+
 window.onload = async function() {
-    document.getElementById('text-container').innerHTML = "";
-    var content = "Lorem ipsum dolor sit amet consectetur</span>, adipisicing elit. Optio quia tenetur odit, magni hic beatae unde neque ipsam! Dolor adipisci illum possimus excepturi at iste explicabo deserunt amet odit ullam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam neque laudantium vel non expedita eum a nobis veniam impedit nam. Expedita, dolor corporis libero at ad quas autem veniam sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam blanditiis ipsa molestias unde quis! Fugiat eos maxime cum, necessitatibus perferendis architecto, recusandae est soluta, excepturi accusamus quidem qui fuga unde."
-    var current = 0;
-    var words = content.split(" ");
+    document.getElementById('text-container').innerHTML = content;
+    score_update = setInterval(updateScore, score_speed/(streak*10));
+}
 
-    var text = document.getElementById('text-container');
-    for(var current = 0; current < words.length; current++) {
-        text.innerHTML = "";
-        for(var i = 0; i < current; i++) {
-            text.innerHTML += words[i];
-            text.innerHTML += " ";
+function updateScore() {
+    console.log(score, speed);
+    if(score_posted < score) {
+        score_posted+= (streak * 2);
+        document.getElementById('score').innerHTML = score_posted;
+    } else {
+        if(end_game) {
+            clearInterval(score_update);
         }
-
-        text.innerHTML += "<span style='color:aqua';font-weight:800;>" + words[current] + " </span>";
-
-        for(var i = current + 1; i < words.length; i++) {
-            text.innerHTML += words[i];
-            text.innerHTML += " ";
-        }
-        
-        await sleep(300);        
+    }
+    if(end_game) {
+        clearInterval(timer);
+        document.getElementById('text-container').innerHTML = content;
     }
 }
 
-function sleep(time) {
-    return new Promise((resolve, reject) => {
-        console.log("time stopped");
-        setTimeout(resolve, time);
-    })
+function wordTyped(event) {
+    if(words[current].match("^" + event.target.value) == null && event.target.value !== "") {
+        color="#e53935";
+    } else {
+        color="#3f51b5";
+        streak=1;
+    }
+    changeText();
+    if(event.target.value[event.target.value.length - 1] === " ") {
+        // event.target.value = event.target.value.trim();
+        color = "#3f51b5";
+        spaceTyped();
+    }
+    if(current >= words.length - 1) {
+        spaceTyped();
+    }
+    if(start_game == false) {
+        start_game = true;
+        timer = setInterval(tick, 1000);
+    }
+    calculateWPM();
+}
+
+function calculateWPM() {
+    var characters = 0;
+    for(var i = 0; i < current; i++) {
+        characters += words[i].length;
+    }
+    characters += current;
+    if(seconds < 1) {
+        speed = 0;
+    } else {
+        speed = (characters * 60/(seconds * 5)).toFixed(2);
+    }
+    document.getElementById('speed_counter').innerHTML = speed;
+}
+
+function tick() {
+    seconds++;
+}
+
+function spaceTyped() {
+    console.log("space typed");
+    var text = document.getElementById('text-container');
+    var input = document.getElementById('type');
+    console.log("word: " + input.value + words[current]);
+    var match = false;
+    if(current >= words.length - 1) {
+        if(input.value === words[current]) {
+            match = true;
+        }
+    }
+    
+    if(input.value.slice(0, input.value.length - 1) === words[current] || match) {
+        console.log("matched");
+        input.value = "";
+        current++;
+        changeText();
+        streak++;
+        score += speed * streak;
+        if(current == words.length) {
+            end_game = true;
+        }
+    } else {
+        console.log("not matched");
+    }
+}
+
+function changeText() {
+    var text = document.getElementById('text-container');
+    text.innerHTML = "";
+    if(current >= words.length){
+        return;
+    }
+
+    for(var i = 0; i < current; i++) {
+        text.innerHTML += words[i];
+        text.innerHTML += " ";
+    }
+
+    text.innerHTML += "<span style='background-color:"+ color +"';font-weight:800;>" + words[current] + " </span>";
+
+    for(var i = current + 1; i < words.length; i++) {
+        text.innerHTML += words[i];
+        text.innerHTML += " ";
+    }
 }
